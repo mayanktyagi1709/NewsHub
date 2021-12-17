21e6a3c8250c4fbfc092ee7b936df99ebb62423f
mayanktyagi1709 <mayanktyagi1709@gmail.com>
2021-12-17 22:05:10 +0530

    Fixed Loading Bug

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,26 +1,23 @@
-import React, {useState, useEffect} from "react";
-import PropTypes from 'prop-types'
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
 import InfiniteScroll from "react-infinite-scroll-component";
 
 const News = (props) => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
 
-  const [articles, setArticles] = useState([])
-  const [loading, setLoading] = useState(true)
-  const [page, setPage] = useState(1)
-  const [totalResults, setTotalResults] = useState(0)
-  
-  const capitalize = (string)=>{
+  const capitalize = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
-  }
+  };
 
-  document.title = `${capitalize(props.category)} - NewsHub`;
-
-  const updateNews = async ()=>{
+  const updateNews = async () => {
     props.setProgress(10);
     const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
-    setLoading(true)
+    setLoading(true);
     // fetching url using fetch() API
     let data = await fetch(url);
     props.setProgress(30);
@@ -29,40 +26,41 @@ const News = (props) => {
     // setting the state of "articles" as parsedData
     props.setProgress(70);
 
-    setArticles(parsedData.articles)
-    setTotalResults(parsedData.totalResults)
-    setLoading(false)
+    setArticles(parsedData.articles);
+    setTotalResults(parsedData.totalResults);
+    setLoading(false);
 
     props.setProgress(100);
-
-  }
+  };
   useEffect(() => {
+    document.title = `${capitalize(props.category)} - NewsHub`;
     updateNews();
     // eslint-disable-next-line
-  }, [])
- 
+  }, []);
 
-  const fetchMoreData = async()=>{
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page+1}&pageSize=${props.pageSize}`;
-    setPage(page+1)
+  const fetchMoreData = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page + 1}&pageSize=${props.pageSize}`;
+    setPage(page + 1);
     let data = await fetch(url);
     let parsedData = await data.json();
 
-    setArticles(articles.concat(parsedData.articles))
-    setTotalResults(parsedData.totalResults)
-  }
+    setArticles(articles.concat(parsedData.articles));
+    setTotalResults(parsedData.totalResults);
+  };
 
   return (
     <>
-      <h1 className="text-center" style={{margin:"40px", marginTop: "90px"}}>Top Headlines - {capitalize(props.category)}</h1>
-      <hr/>
-      {loading && <Spinner/>}
+      <h1 className="text-center" style={{ margin: "40px", marginTop: "90px" }}>
+        Top Headlines - {capitalize(props.category)}
+      </h1>
+      <hr />
+      {loading && <Spinner />}
 
       <InfiniteScroll
         dataLength={articles.length}
         next={fetchMoreData}
         hasMore={articles.length !== totalResults}
-        loader={<Spinner/>}
+        loader={<Spinner />}
       >
         <div className="container y-3">
           <div className="row">
@@ -86,19 +84,18 @@ const News = (props) => {
       </InfiniteScroll>
     </>
   );
-
-}
+};
 
 News.defaultProps = {
   pageSize: 6,
-  country: 'in',
-  category: 'general'
-}
+  country: "in",
+  category: "general",
+};
 
 News.propTypes = {
   pageSize: PropTypes.number,
   country: PropTypes.string,
-  category: PropTypes.string
-}
+  category: PropTypes.string,
+};
 
-export default News
+export default News;
