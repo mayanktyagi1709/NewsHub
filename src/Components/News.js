f7315e548fed447be78d345c4253f96842061eef
mayanktyagi1709 <mayanktyagi1709@gmail.com>
2021-12-17 18:30:09 +0530

    Added environment variable

--- src/Components/News.js
+++ src/Components/News.js
@@ -33,7 +33,7 @@ export default class News extends Component {
 
   updateNews= async ()=>{
     this.props.setProgress(10);
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=0d5e789c790247199b9152d13f945bd9&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({
       loading: true
     })
@@ -60,7 +60,7 @@ export default class News extends Component {
 
   fetchMoreData = async()=>{
     this.setState({page: this.state.page+1});
-    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=0d5e789c790247199b9152d13f945bd9&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     let parsedData = await data.json();
     this.setState({ 
